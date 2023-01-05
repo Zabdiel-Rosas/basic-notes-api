1ee20cabb155312f5c255a92e06e26b7ed91423b
Zabdiel Rosas <zabdiel_rosas@outlook.com>
2023-01-04 23:56:02 -0600

    feat(Express Server): express is used to make the web server instead of plain node

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
 let notes = [
   {
@@ -21,11 +22,18 @@ let notes = [
   }
 ]
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'application/json' })
-  response.end(JSON.stringify(notes))
+// Routes
+// Root Resource
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+// Get All Notes Resource
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
 })
 
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
